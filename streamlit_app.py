341bcc8b379b3b44d26bdb43aca8a4f8e26ecca0
Foxhunting0 <zoahaji@gmail.com>
2024-07-25 16:04:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,20 @@
 import streamlit as st
 import requests
+import os
 
 st.title("🎈 My new app")
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io)")
 
-video_path = st.text_input("비디오 경로를 입력하세요:")
+# 동영상 파일 업로드
+uploaded_file = st.file_uploader("동영상 파일을 업로드하세요", type=["mp4", "avi", "mov"])
 
 if st.button("멧돼지 감지 시작"):
-    if video_path:
+    if uploaded_file is not None:
+        # 임시 파일로 저장
+        video_path = f"./{uploaded_file.name}"
+        with open(video_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        
         # Flask 서버에 요청 보내기
         url = "http://localhost:5000/detect"  # Flask 서버 URL
         response = requests.post(url, json={"video_path": video_path})
@@ -21,5 +28,8 @@ if st.button("멧돼지 감지 시작"):
                 st.warning(data.get("message"))
         else:
             st.error("서버 오류가 발생했습니다.")
+        
+        # 임시 비디오 파일 삭제
+        os.remove(video_path)
     else:
-        st.error("비디오 경로를 입력해야 합니다.")
+        st.error("동영상 파일을 업로드해야 합니다.")
